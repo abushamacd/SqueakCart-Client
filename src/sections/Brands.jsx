f4b68de1e11cbf41acae4906c7dd8e29bab518cc
shama <assiddik001@gmail.com>
2023-07-26 18:23:18 +0600

    show brands in homepage

--- src/sections/Brands.jsx
+++ src/sections/Brands.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import Marquee from "react-fast-marquee";
+import { useGetBrandsQuery } from "../redux/features/brand/brandApi";
+import Loading from "../components/Loading";
 
 const Brands = () => {
+  const { data: getData, isLoading: getIsLoading } = useGetBrandsQuery();
+  const brands = getData?.data?.data;
+  console.log(brands);
+  if (getIsLoading) {
+    return <Loading />;
+  }
   return (
     <div className="box_shadow p-[15px] bg-white rounded-xl section_gap">
       <Marquee className="flex items-center">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((brand) => (
-          <div key={brand} className="brans mx-[15px]">
-            <img src="/images/brand-02.png" alt="brand" />
+        {brands.map((brand) => (
+          <div key={brand._id} className="brands mx-[15px]">
+            <img className="h-[120px]" src={brand?.images[0].url} alt="brand" />
           </div>
         ))}
       </Marquee>
